940be5dfa5c8fd562373dd65d8a8c9a86e8748c5
Nazim Boudeffa <boudeffa@gmail.com>
2024-02-03 17:01:47 +0100

    removed svg

--- src/components/Problem.tsx
+++ src/components/Problem.tsx
@@ -14,32 +14,6 @@ export default function Problem() {
             </p>
             </div>
         </div>
-        <svg
-            viewBox="0 0 1024 1024"
-            className="absolute left-1 top-0 -z-10 h-[64rem] w-[64rem] [mask-image:radial-gradient(closest-side,white,transparent)]"
-            aria-hidden="true"
-        >
-            <circle cx={512} cy={512} r={512} fill="url(#top)" fillOpacity="0.7" />
-            <defs>
-            <radialGradient id="top">
-                <stop stopColor="#00FFEE" />
-                <stop offset={1} stopColor="#00FFEE" />
-            </radialGradient>
-            </defs>
-        </svg>
-        <svg
-            viewBox="0 0 1024 1024"
-            className="absolute left-1/2 top-3/4 -z-10 h-[64rem] w-[64rem] -translate-y-1/2 [mask-image:radial-gradient(closest-side,white,transparent)] sm:left-full sm:-ml-80 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2 lg:translate-y-0"
-            aria-hidden="true"
-        >
-            <circle cx={512} cy={512} r={512} fill="url(#down)" fillOpacity="0.7" />
-            <defs>
-            <radialGradient id="down">
-                <stop stopColor="#00FF00" />
-                <stop offset={1} stopColor="#00FF00" />
-            </radialGradient>
-            </defs>
-        </svg>
         </section>
     )
 }
